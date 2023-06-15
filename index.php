9078e3fea6a7e77fda3f3fdedcf6955ae24c6630
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-06-15 11:47:21 +0200

    ThirdCommit

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <div class="container-fluid bkgBrown">
   <!-- Menu -->
   <div class="row">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
     </a>
     <a class="navbar-brand" href="#">
@@ -25,9 +25,12 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
+        <ul class="navbar-nav px-3">
+          <li class="nav-item active activeGradient">
             <a class="nav-link" href="#">Enchères <span class="sr-only">(ici)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#">Enchères Terminées</a>
           </li>
           <li class="nav-item">
             <a class="nav-link disabled" href="#">Mon Profil</a>
@@ -39,17 +42,15 @@
             <a class="nav-link disabled" href="#">Mes Annonces</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link " href="#">Inscription/Connexion</a>
+            <a class="nav-link " href="#">Login</a>
           </li>
           
         </ul>
       </div>
-    </nav>
-  </div>
-</nav>
-
-  </div>
-  <!-- Body de l'index -->
+  </nav>
+</div>
+  
+    <!-- Body de l'index -->
   <div class="row">
     <!-- Colonne de gauche -->
     <div class="col-2">
@@ -57,16 +58,20 @@
     </div>
     <!-- Colonne de droite -->
     <div class="col-10">
-      
+      <div class="row">
+        <p class="colorWhite d-flex justify-content-center">Enchères Actives</p>
+      </div>
+      <!-- Content Cards -->
+      <div class="bkgImg row">
+      </div>
+        
     </div>
   </div>
 </div>
-  <?php
-
-    ?>
+  
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<footer class="bg-dark d-flex justify-content-center colorWhite"><p>Copyright CBNC</p></footer>
+<footer class="fixed-bottom bg-dark d-flex justify-content-center colorWhite"><p>Copyright CBNC</p></footer>
 </body>
 </html>
